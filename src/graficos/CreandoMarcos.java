f3593cb8e0b7abc849752854c8c2776a44e5c10a
Francisco Javier <43383605+XXXKaos@users.noreply.github.com>
2019-05-23 14:55:03 -0500

    changes applied
    Changed name pildoras > to Java repository name, and make this repository public.

--- src/graficos/CreandoMarcos.java
+++ src/graficos/CreandoMarcos.java
@@ -6,15 +6,18 @@ public class CreandoMarcos {
 	
 	public static void main(String[] args) {
 		miFrame frame = new miFrame();
-		frame.setVisible(true);
+		frame.setVisible(true); //hacer visible
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Cuando la ventana se cierra, se acaba el programa
+		
+
 	}
 }
 
 
+@SuppressWarnings("serial")
 class miFrame extends JFrame{
 	
 	//CONSTRUCTOR
-	
 	public miFrame() {
 		setSize(500,300);
 	}
